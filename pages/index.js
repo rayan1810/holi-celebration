398e60480b820b9c013b2c4477dde2c715d068df
Rohit Singh <rohit99.ind@gmail.com>
2022-03-18 03:03:26 +0530

    initial setup

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Animated, Easing } from "react-native";
 import {
   Center,
   useColorMode,
@@ -10,88 +11,158 @@ import {
   HStack,
   Text,
   Heading,
-  Code,
+  Modal,
   Link,
-  VStack,
+  Box,
   Button,
   AspectRatio,
+  ZStack,
+  Circle,
+  VStack,
 } from "native-base";
 
 // Start editing here, save and see your changes.
 export default function App() {
+  const [rotationDeg, setRotationDeg] = React.useState("400");
+  const [showModal, setShowModal] = React.useState(false);
+  const [luckyColor, setLuckyColor] = React.useState("");
+  let rotateValueHolder = new Animated.Value(0);
+  const getRandomNumber = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
+  const startImageRotateFunction = () => {
+    console.log(rotationDeg);
+    // setRotationDeg(getRandomNumber(400, 1600).toString());
+    rotateValueHolder.setValue(0);
+    Animated.timing(rotateValueHolder, {
+      toValue: 1,
+      duration: 3000,
+      easing: Easing.ease,
+      useNativeDriver: false,
+    }).start(() => {
+      setShowModal(true);
+      setLuckyColor(colorPalette[Math.floor((rotationDeg % 360) / 45)]);
+    });
+  };
+
+  let rotateData = rotateValueHolder.interpolate({
+    inputRange: [0, 1],
+    outputRange: ["0deg", rotationDeg + "deg"],
+  });
+
+  const isInitialMount = React.useRef(true);
+  const colorPalette = [
+    "orange",
+    "green",
+    "pink",
+    "yellow",
+    "rose",
+    "info",
+    "blue",
+    "violet",
+  ];
+  React.useEffect(() => {
+    if (isInitialMount.current) {
+      isInitialMount.current = false;
+    } else {
+      startImageRotateFunction();
+      console.log(Math.floor((rotationDeg % 360) / 45));
+      console.log(colorPalette[Math.floor((rotationDeg % 360) / 45)]);
+    }
+  }, [rotationDeg]);
   return (
-    <Center
-      flex={1}
-      _dark={{ bg: "blueGray.900" }}
-      _light={{ bg: "blueGray.50" }}
-    >
-      <VStack alignItems="center" space="md">
-        <HStack alignItems="center" space="2xl">
-          <AspectRatio w={24} ratio={1.66}>
+    <Center flex={1} bg="muted.900" position="relative">
+      {/* <AspectRatio h="100%" w="100%"> */}
+      {/* <Image
+        h="100%"
+        w="100%"
+        resizeMode="cover"
+        source={{
+          uri: "https://images.unsplash.com/photo-1478088702756-f16754aaf0c4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+        }}
+      /> */}
+      {/* </AspectRatio> */}
+      <Box
+        position="absolute"
+        bottom="0"
+        top="0"
+        right="0"
+        left="0"
+        flex="1"
+        justifyContent="center"
+        alignItems="center"
+        overflow="hidden"
+        zIndex={-1}
+      >
+        <ZStack justifyContent="center" alignItems="center">
+          <Circle bg="#120E0E" size="1200px" />
+          <Circle bg="black" size="600px" />
+        </ZStack>
+      </Box>
+      <VStack space="8" alignItems="center">
+        <Heading fontSize="40px" color="warmGray.100">
+          FIND YOUR COLOR FOR THIS HOLI
+        </Heading>
+        <ZStack justifyContent="center" alignItems="center" size="500">
+          <Box>
+            <Animated.View style={{ transform: [{ rotate: rotateData }] }}>
+              <AspectRatio ratio={1} w="650">
+                <Image
+                  resizeMode="cover"
+                  source={{
+                    uri: "/images/SpinnerBoard.png",
+                  }}
+                />
+              </AspectRatio>
+            </Animated.View>
+          </Box>
+          <AspectRatio ratio={1} w="150" mt="-8">
             <Image
-              source={{ uri: "images/nextjs-logo.png" }}
-              alt="NextJS Logo"
               resizeMode="contain"
+              source={{
+                uri: "/images/SpinTop.png",
+              }}
             />
           </AspectRatio>
-          <Text fontSize="4xl">+</Text>
-          <Image
-            source={{ uri: "images/nativebase-logo.svg" }}
-            alt="NativeBase Logo"
-            size={24}
-            resizeMode="contain"
-          />
-        </HStack>
-        <Heading>Welcome to NativeBase</Heading>
-        <Text>
-          Edit <Code>src/pages/index.js</Code> and save to reload.
-        </Text>
-        <HStack alignItems="center" space="sm">
-          <Link href="https://nextjs.org/docs/getting-started" isExternal>
-            <Text
-              _light={{ color: "gray.700" }}
-              _dark={{ color: "gray.400" }}
-              underline
-              fontSize={"xl"}
-            >
-              Learn NextJS
-            </Text>
-          </Link>
-          <Text>/</Text>
-          <Link href="https://docs.nativebase.io" isExternal>
-            <Text color="primary.500" underline fontSize={"xl"}>
-              Learn NativeBase
-            </Text>
-          </Link>
-        </HStack>
-      </VStack>
-      <ColorModeSwitch />
-      <Link mt="6" href="https://docs.nativebase.io" isExternal>
-        <Button variant="outline" colorScheme="coolGray">
-          View Repo
+        </ZStack>
+        <Button
+          onPress={() => {
+            setRotationDeg(getRandomNumber(400, 1600).toString());
+          }}
+          variant="unstyled"
+          borderWidth={1}
+          borderColor="warmGray.100"
+          w="140"
+          _text={{ color: "warmGray.100" }}
+        >
+          SPIN THE WHEEL
         </Button>
-      </Link>
+      </VStack>
+      <Modal isOpen={showModal} onClose={setShowModal}>
+        <Modal.Content>
+          <Modal.CloseButton />
+          {/* <Modal.Header>Your Color for this Holi is </Modal.Header> */}
+          <Modal.Body bg="muted.900" px="8" py="6">
+            <Text color="warmGray.100" fontSize="lg" mr="6">
+              Yay! Your Color for this Holi is...
+            </Text>
+            <HStack
+              my="8"
+              space="8"
+              alignItems={"center"}
+              justifyContent="center"
+            >
+              <Box rounded={16} bg={`${luckyColor}.500`} size="20"></Box>
+              <Heading color="muted.100">
+                {luckyColor.charAt(0).toUpperCase() + luckyColor.slice(1)} 🎉
+              </Heading>
+            </HStack>
+            <Text color="muted.100">
+              Checkout what we have for you as a Gift <Link>here</Link>
+            </Text>
+          </Modal.Body>
+        </Modal.Content>
+      </Modal>
     </Center>
-  );
-}
-// Color Switch Component
-function ColorModeSwitch() {
-  const { colorMode, toggleColorMode } = useColorMode();
-  return (
-    <Tooltip
-      label={colorMode === "dark" ? "Enable light mode" : "Enable dark mode"}
-      placement="bottom right"
-      openDelay={300}
-      closeOnClick={false}
-    >
-      <IconButton
-        position="absolute"
-        top={12}
-        right={8}
-        onPress={toggleColorMode}
-        icon={colorMode === "dark" ? <SunIcon /> : <MoonIcon />}
-        accessibilityLabel="Color Mode Switch"
-      />
-    </Tooltip>
   );
 }
